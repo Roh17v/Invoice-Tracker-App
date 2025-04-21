d7e9f7d64dd3ace30f6255ef000e60db377c1347
Rohit Verma <rohit.verma272727@gmail.com>
2025-04-21 15:24:28 +0530

    feat: Install and use bcrypt to hash password before saving

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -26,4 +26,20 @@ const userSchema = new mongoose.Schema({
   },
   { timestamps: true });
 
+  userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await genSalt(10);
+    this.password = await hash(this.password, salt);
+    next();
+  });
+  
+  userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign(
+      { _id: this._id, email: this.email, role: this.role },
+      process.env.JWT_KEY,
+      { expiresIn: "1d" }
+    );
+    return token;
+  };
+
   export const UserSchema = mongoose.model("User", userSchema);
